72a90ce8cec7966c0ebaadf92b4cfa405efbfedc
Rahi6023 <rahidulmowlarahi6023@gmail.com>
2026-02-14 17:13:00 +0600

    Initial Commit

---
+++ 21. Aside- FormDate methods/index.js
@@ -0,0 +1,17 @@
+const loginForm = document.getElementById('login-form')
+
+loginForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    const loginFormData = new FormData(loginForm)
+    
+    const name = loginFormData.get('astronautName')
+    console.log(name) 
+    
+/* 
+Challenge: 
+1. Log out the email and password from loginFormData.
+*/
+    const email = loginFormData.get('astronautEmail')
+    console.log(email)
+
+})
